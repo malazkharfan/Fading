a81a91aec83b1d71866e9ab72a6cfa375e31e8ea
Malaz Kharfan <malazkharfan@outlook.com>
2021-10-21 16:20:34 +0200

    README.md / Code added

---
+++ Code/Code.ino
@@ -0,0 +1,16 @@
+//Fading
+//
+// External LED should be fading after being plugged in.
+
+// Variables 
+int ledPin=6;
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly
+
+}
