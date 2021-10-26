2facbd82fdb02902d501fbf8567a5377dd632a8d
Malaz Kharfan <malazkharfan@outlook.com>
2021-10-26 16:07:51 +0200

    Code organize/ comments has been added

--- Code/Code.ino
+++ Code/Code.ino
@@ -3,28 +3,30 @@
 // External LED should be fading after being plugged in.
 
 // Variables 
-const int ledPin=6;
+const int LED_PIN=6;
 const int MAX_BRIGHTNESS=255;
 const int MIN_BRIGHTNESS=0;
+const int WAIT=30;
 int brightness=0;
-int fadeAmount=1;
+int fadeAmount=-1;
 
 
 void setup() {
   // pin6 has been declared as an output.
-  pinMode(ledPin, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  // ledPin will start as 0, which is the brightness value. 
-  analogWrite(ledPin, brightness);
+  // if the brightness is greater/equal to Max_Brightness or if the brightness is less/equal to Min_Brightness.
+  if (brightness >= MAX_BRIGHTNESS || brightness <=MIN_BRIGHTNESS) {
+     fadeAmount= -fadeAmount; // then fadeAmount =-1*fadeAmount.
+  }
 
-  //change the brightness
+  // the brightness is the brightness variable + the fadeAmount.
   brightness=brightness+fadeAmount;
+    
+  // LED_PIN will light based on the brightness vlaue
+  analogWrite(LED_PIN, brightness);
 
-  //||
-    if (brightness >= MAX_BRIGHTNESS || brightness <=MIN_BRIGHTNESS) {
-      fadeAmount= -fadeAmount;
-    }
-    delay(30);
+  delay(WAIT); // wait time between every pulse
 }
