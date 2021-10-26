ca8c115c4339c9be8ec51b9bd23bb8c7aa1b1804
Malaz Kharfan <malazkharfan@outlook.com>
2021-10-26 13:10:05 +0200

    Code Before Organizing

--- Code/Code.ino
+++ Code/Code.ino
@@ -1,16 +1,30 @@
-//Fading
+// Fading
 //
 // External LED should be fading after being plugged in.
 
 // Variables 
-int ledPin=6;
+const int ledPin=6;
+const int MAX_BRIGHTNESS=255;
+const int MIN_BRIGHTNESS=0;
+int brightness=0;
+int fadeAmount=1;
 
 
 void setup() {
-  // put your setup code here, to run once:
+  // pin6 has been declared as an output.
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly
+  // ledPin will start as 0, which is the brightness value. 
+  analogWrite(ledPin, brightness);
 
+  //change the brightness
+  brightness=brightness+fadeAmount;
+
+  //||
+    if (brightness >= MAX_BRIGHTNESS || brightness <=MIN_BRIGHTNESS) {
+      fadeAmount= -fadeAmount;
+    }
+    delay(30);
 }
